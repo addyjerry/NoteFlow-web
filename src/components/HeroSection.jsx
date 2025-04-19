9b998fba4056ff150cf15bb2f219c6e0f0771baf
Bishop <addyjerry2704@gmail.com>
2025-04-19 23:02:04 +0000

    HeroSection update

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import MyButton from "./MyButton";
 import Notes from "../assets/images/Summarizer.webp";
-import avatar1 from "../assets/images/avatar1.webp";
-import avatar2 from "../assets/images/avatar2.webp";
-import avatar3 from "../assets/images/avatar3.webp";
-import avatar4 from "../assets/images/avatar4.webp";
+import avatar1 from "../assets/images/avatar4.webp";
+import avatar2 from "../assets/images/avatar3.webp";
+import avatar3 from "../assets/images/avatar2.webp";
+import avatar4 from "../assets/images/avatar1.webp";
 import avatar5 from "../assets/images/avatar5.webp";
 
 const HeroSection = () => {
@@ -16,7 +16,7 @@ const HeroSection = () => {
           <h1 className="font-bold text-5xl/15 w-xl py-1">
             AI-Powered Notes. Organize and Summarize in Seconds
           </h1>
-          <p className="text-lg/10 font-300 w-sm py-7">
+          <p className="text-lg/10 text-[rgba(199,247,248,1)] font-300 w-sm py-7">
             Let AI organize & summarize your notes, saving you time and boosting
             productivity
           </p>
@@ -33,9 +33,12 @@ const HeroSection = () => {
       </div>
       <div className="flex ">
         {pictures.map((picture) => (
-          <img src={picture} alt="avatar" className="w-10 ml-[-1rem]" />
+          <img src={picture} alt="avatar" className="w-10 ml-[-0.9rem]" />
         ))}
-        <p className="px-10">Trusted by 12,653+ productivity junkies</p>
+        <p className="px-10 py-2">
+          Trusted by <b className="text-[#44e5e7]/100">12,653+</b> productivity
+          junkies
+        </p>
       </div>
     </section>
   );
