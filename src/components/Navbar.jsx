effcd82900bebd08c6b760e463912b29711250c9
Bishop <addyjerry2704@gmail.com>
2025-04-19 09:12:19 +0000

    header design

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul className="flex list-none gap-7">
+        <li>Home</li>
+        <li>Pricing</li>
+        <li>About</li>
+        <li>Community</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
