34287764b74170592a913402fa33b9c4e72604ed
Bishop <addyjerry2704@gmail.com>
2025-04-20 20:59:38 +0000

    first commit to questionsection

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import HeroSection from "./components/HeroSection";
 import Organizations from "./components/Organizations";
 import ToolSection from "./components/ToolSection";
+import QuestionSection from "./components/QuestionSection";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <HeroSection />
       <Organizations />
       <ToolSection />
+      <QuestionSection />
     </div>
   );
 }
---
+++ src/components/Questions/data.js
@@ -0,0 +1,33 @@
+import Note from "../../assets/images/Note.webp";
+import Edit from "../../assets/images/edit.webp";
+import Contact from "../../assets/images/contact.webp";
+import Security from "../../assets/images/security.webp";
+
+export const questions = [
+  {
+    id: 1,
+    pic: Note,
+    question: "What is NoteFlow?",
+    answer:
+      "NoteFlow is an AI-powered note-taking application designed to help individuals and teams organise,manage and retrieve notes effortlessly.",
+  },
+  {
+    id: 2,
+    pic: Edit,
+    question: "How does NoteFlow use AI to enhance Note taking",
+    answer:
+      "The Noteflow has an advanced AI that helps to take notes and record events",
+  },
+  {
+    id: 3,
+    pic: Contact,
+    question: "Can NoteFlow be used for team collaboration?",
+    answer: "Yes , you can connect with others while you use NoteFlow",
+  },
+  {
+    id: 4,
+    pic: Security,
+    question: "Is my data secure with NoteFlow?",
+    answer: "Yes your data is fully secured with NoteFlow",
+  },
+];
